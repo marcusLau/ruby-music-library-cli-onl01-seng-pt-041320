d5a00907f2660fc5a3adbedd082f10bb6e45cd49
marcusLau <lau.marcus76@gmail.com>
2020-05-29 15:50:30 -0700

    Done.

--- lib/musicLibraryController.rb
+++ lib/musicLibraryController.rb
@@ -23,13 +23,67 @@ class MusicLibraryController
             input = gets.chomp
             case input 
             when "list songs"
-                Song.all
+                list_songs
             when "list artists"
-                Artist.all
+                list_artists
+            when "list genres"
+                list_genres
             when "exit"
                 running = false
             end
         end
     end
+    
+    # prints all songs in music library in a numbered list alphabetized by song name
+    def list_songs
+        Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |x, i|
+            puts "#{i+1}. #{x.artist.name} - #{x.name} - #{x.genre.name}"
+        end
+    end
 
+    def list_artists
+        Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |x, i|
+            puts "#{i+1}. #{x.name}"
+        end
+    end
+
+    def list_genres
+        Genre.all.sort{|a, b| a.name <=> b.name}.each_with_index do |x, i|
+            puts "#{i+1}. #{x.name}"
+        end
+    end
+
+    def list_songs_by_artist
+        puts "Please enter the name of an artist:"
+        input = gets.chomp
+        if artist_name = Artist.find_by_name(input)
+            artist_name.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, i|
+                puts "#{i+1}. #{song.name} - #{song.genre.name}"
+            end
+        end
+    end
+
+    def list_songs_by_genre
+        puts "Please enter the name of a genre:"
+        input = gets.chomp
+        if genre_name = Genre.find_by_name(input)
+            genre_name.songs.sort{|a, b| a.name <=> b.name}.each_with_index do |song, i|
+                puts "#{i+1}. #{song.artist.name} - #{song.name}"
+            end
+        end
+    end
+
+
+    def play_song
+        puts "Which song number would you like to play?"
+        input = gets.chomp.to_i
+        if input > 1 && input < Song.all.length
+            Song.all.sort{|a, b| a.name <=> b.name}.each_with_index do |song, i|
+                if input == i+1 
+                    puts "Playing #{song.name} by #{song.artist.name}"
+                end
+            end
+        end
+    end
+    
 end
